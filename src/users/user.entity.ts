09995f0fc22c3a875599511a7910095e457cfe3c
Tigran Kostandyan <t.kostandyan1@gmail.com>
2020-02-05 01:49:36 +0300

    Specify users service, main DTO's and start writing auth service

---
+++ src/users/user.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+} from 'typeorm';
+import {IsEmail} from 'class-validator';
+import * as bcrypt from 'bcrypt';
+import { UserDto } from './user.dto';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsEmail()
+  @Column({length: 100})
+  email: string;
+
+  @Column({length: 100})
+  username: string;
+
+  @Column({length: 100})
+  firstName: string;
+
+  @Column({length: 100})
+  lastName: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async comparePassword(attempt: string): Promise<any> {
+    return await bcrypt.compare(attempt, this.password);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { UserDto } from './user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  public async getUsers(): Promise<any> {
+    return await this.userRepository.find();
+  }
+  public async getByEmail(userEmail: string): Promise<any | undefined> {
+    return await this.userRepository.findOne({email: userEmail});
+  }
+  public async getByUsername(username: string): Promise<any | undefined> {
+    return await this.userRepository.findOne({where: {username}});
+  }
+  public async createUser(newUser: UserDto): Promise<any> {
+    const { email } = newUser;
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (user) {
+      throw new HttpException(
+        'User already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.userRepository.save(user).then(record => ({id: record.id}));
+  }
+  public async updateUser(id: number, user: UserDto): Promise<any> {
+    return await this.userRepository.update(id, user);
+  }
+  public async deleteUser(id: number): Promise<any> {
+    return await this.userRepository.delete(id);
+  }
+}
